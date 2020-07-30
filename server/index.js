7a5fd7952cefb9f05c5a921023ecf0f2fdd71924
zemitda79 <zemitda79@gmail.com>
2020-07-30 20:20:10 +0900

    22강까지

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const bodyParser = require('body-parser');
 const { auth } = require('./middleware/auth');
 const { user } = require('./models/user');
@@ -27,6 +27,11 @@ mongoose.connect(config.mongoURI, {
 
 
 app.get('/', (req, res) => res.send('Hello World!! 안녕하세요.'))
+
+app.get('/api/hello', (req, res)=> {
+    res.send("안녕하세요.");
+
+})
 
 app.post('/api/users/register', (req,res) => {
     // 회원가입 할때 필요한 정보들을 Client 에서 가져오면
